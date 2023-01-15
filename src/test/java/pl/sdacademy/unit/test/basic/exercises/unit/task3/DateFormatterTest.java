f3c95baff46e0cbc6aa2a9d9085e2e9b61cce999
Pawel Dudko <paweldudko@outlook.com>
2023-01-15 14:11:37 +0100

    junit:ex3 solution

---
+++ src/test/java/pl/sdacademy/unit/test/basic/exercises/unit/task3/DateFormatterTest.java
@@ -0,0 +1,25 @@
+package pl.sdacademy.unit.test.basic.exercises.unit.task3;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DateFormatterTest {
+
+    @Test
+    void shouldReturnCorrectFormatDate() {
+        //given
+        DateFormatter dateFormatter = new DateFormatter();
+        LocalDate input = LocalDate.of(2012, 12, 20);
+        String expectedResult = "2012-20-12";
+        //when
+        String result = dateFormatter.formatDate(input);
+        //then
+        assertEquals(expectedResult, result); //jUnit
+        assertThat(result).isEqualTo(expectedResult); //assertJ
+    }
+
+}
